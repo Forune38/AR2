002cb9c1d1e24fb4e07a1571d69825fcc8d79f08
BuildTools <unconfigured@null.spigotmc.org>
2026-04-16 09:13:51 +0900

    new

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const onxrloaded = () => {
+    XR8.XrController.configure({
+      imageTargetData: [
+        require('../image_targets/target1.json')],
+    })
+  }
+  window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)
